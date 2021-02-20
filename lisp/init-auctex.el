eab71d64fa64ad33fabb1556907fc06f405e2c52
Nikoli Dryden <ndryden@users.noreply.github.com>
2021-02-20 12:23:08 +0100

    Enable RefTeX.

--- lisp/init-auctex.el
+++ lisp/init-auctex.el
@@ -7,8 +7,11 @@
   (setq TeX-parse-self t)
   (when *is-a-mac*
     (setq TeX-view-program-list '(("Preview" "open %o")))
-    (setq TeX-view-program-selection '((output-pdf "Preview")))))
+    (setq TeX-view-program-selection '((output-pdf "Preview"))))
+  ;; Set up RefTeX.
+  (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+  (setq reftex-plug-into-AUCTeX t))
 
 
 (provide 'init-auctex)
-;;; init-auctec.el ends here
+;;; init-auctex.el ends here
