d48e00664da6f33dcc7d9a1e9a67cbb68a835f8d
Nikoli Dryden <ndryden@users.noreply.github.com>
2025-05-22 01:27:46 -0700

    Add `eglot-restart`.

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -12,5 +12,14 @@
 (with-eval-after-load 'abbrev (diminish 'abbrev-mode))
 (with-eval-after-load 'eldoc (diminish 'eldoc-mode))
 
+(defun eglot-restart ()
+  "Restart Eglot for the current buffer.
+This function shuts down any running Eglot server for the current buffer
+and then starts a new one."
+  (interactive)
+  (when (eglot-current-server)
+    (eglot-shutdown))
+  (eglot))
+
 (provide 'init-eglot)
 ;;; init-eglot.el ends here
