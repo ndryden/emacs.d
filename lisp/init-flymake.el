074b8edce3e4e992ef45cca911549e29a5b481bd
Nikoli Dryden <ndryden@users.noreply.github.com>
2025-10-20 20:31:18 -0700

    Flymake ergonomic improvements.

--- lisp/init-flymake.el
+++ lisp/init-flymake.el
@@ -20,11 +20,25 @@
 (add-hook 'prog-mode-hook 'flymake-mode)
 (add-hook 'text-mode-hook 'flymake-mode)
 
+;; For more convenient navigation.
+(defun flymake-goto-next-error-only (&optional n interactive)
+  "Go to Nth next Flymake error, skipping warnings.
+If `flymake-wrap-around' is non-nil, wraps to top when reaching end."
+  (interactive (list (prefix-numeric-value current-prefix-arg) t))
+  (flymake-goto-next-error n '(:error) interactive))
+(defun flymake-goto-prev-error-only (&optional n interactive)
+  "Go to Nth previous Flymake error, skipping warnings.
+If `flymake-wrap-around' is non-nil, wraps to bottom when reaching top."
+  (interactive (list (prefix-numeric-value current-prefix-arg) t))
+  (flymake-goto-prev-error n '(:error) interactive))
+
 (with-eval-after-load 'flymake
   ;; Provide some flycheck-like bindings in flymake mode to ease transition
   (define-key flymake-mode-map (kbd "C-c ! l") 'flymake-show-buffer-diagnostics)
-  (define-key flymake-mode-map (kbd "C-c ! n") 'flymake-goto-next-error)
-  (define-key flymake-mode-map (kbd "C-c ! p") 'flymake-goto-prev-error)
+  (define-key flymake-mode-map (kbd "C-c ! n") 'flymake-goto-next-error-only)
+  (define-key flymake-mode-map (kbd "C-c ! p") 'flymake-goto-prev-error-only)
+  (define-key flymake-mode-map (kbd "C-c ! N") 'flymake-goto-next-error)
+  (define-key flymake-mode-map (kbd "C-c ! P") 'flymake-goto-prev-error)
   (define-key flymake-mode-map (kbd "C-c ! l") 'flymake-show-buffer-diagnostics)
   (define-key flymake-mode-map (kbd "C-c ! c") 'flymake-start))
 
